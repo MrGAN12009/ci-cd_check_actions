0575942744c07b8c3f12400ef990579e261e2d6d
MrGAN12009 <mrgan12009@gmail.com>
2025-09-25 12:24:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import telebot
+
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Бот работает ✅")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, f"Ты написал: {message.text}")
+
+
+if __name__ == "__main__":
+    print("Бот запущен...")
+    bot.polling(none_stop=True)
